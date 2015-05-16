52a45c53d8427a62d3e154fe305facecb0f67777
edoardo <edoardo@mbp-di-edoardo.homenet.telecomitalia.it>
2015-05-16 18:49:40 +0200

    Removed useless parenthesis from HexPoint

--- src/main/java/it/polimi/ingsw/cg_30/HexPoint.java
+++ src/main/java/it/polimi/ingsw/cg_30/HexPoint.java
@@ -13,7 +13,7 @@ public class HexPoint {
 	}
 
 	public int getCubicZ() {
-		return (-cubicX - cubicY);
+		return -cubicX - cubicY;
 	}
 
 	public int getAxialX() {
@@ -21,7 +21,7 @@ public class HexPoint {
 	}
 
 	public int getAxialY() {
-		return (-cubicX - cubicY);
+		return -cubicX - cubicY;
 	}
 
 	public int getOffsetX() {
@@ -29,27 +29,27 @@ public class HexPoint {
 	}
 
 	public int getOffsetY() {
-		return (((-cubicX - cubicY) + (cubicX - (cubicX & 1)) / 2));
+		return -cubicX - cubicY + (cubicX - (cubicX & 1)) / 2;
 	}
-	
-	//"COSTRUTTORI"
-	private HexPoint hexPoint(int x, int y){
+
+	// "COSTRUTTORI"
+	private HexPoint hexPoint(int x, int y) {
 		HexPoint ex = new HexPoint();
-		this.cubicX=x;
-		this.cubicY=y;
+		this.cubicX = x;
+		this.cubicY = y;
 		return ex;
 	}
-	
-	public HexPoint hexPointCubic(int x, int y){
+
+	public HexPoint hexPointCubic(int x, int y) {
 		return hexPoint(x, y);
 	}
-	
-	public HexPoint hexPointAxial(int q, int r){
-		return hexPoint(q, (-q-r));
+
+	public HexPoint hexPointAxial(int q, int r) {
+		return hexPoint(q, -q - r);
 	}
-	
-	public HexPoint hexPointOffset(int col, int row){
-		return hexPoint(col, (-col-(row-(col-(col&1))/2)));
+
+	public HexPoint hexPointOffset(int col, int row) {
+		return hexPoint(col, -col - (row - (col - (col & 1)) / 2));
 	}
 
 }
