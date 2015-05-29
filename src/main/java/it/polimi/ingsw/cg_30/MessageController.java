b65c822352213c41181dd9ac483976c4fc0944c5
LuKePicci <luca.piccirillo@gmail.com>
2015-05-29 14:06:46 +0200

    Partial Message delivery routines definition

--- src/main/java/it/polimi/ingsw/cg_30/MessageController.java
+++ src/main/java/it/polimi/ingsw/cg_30/MessageController.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.cg_30;
 
+import javax.naming.OperationNotSupportedException;
+
 public class MessageController {
 
     private AcceptPlayer myAP; // Bind the connected player
@@ -10,29 +12,40 @@ public class MessageController {
         this.myAP = ap;
     }
 
-    public void deliver(Message msg) {
-        switch (msg.getType()) {
-            case ChatMessage:
-                break;
-
-            case JoinMessage:
-                myParty = PartyController.processJoinRequest(myAP,
-                        ((JoinMessage) msg).getContent());
-                break;
-
-            case ActionMessage:
-                if (isJoined()) {
-
-                }
-                break;
-
-            case PartyMessage:
-                if (isJoined()) {
-
-                }
-                break;
-
+    /**
+     * Deliver a generic message to its target.
+     *
+     * @param msg
+     *            the message to be delivered
+     */
+    public synchronized void deliver(Message msg) {
+        RequestModel rq = msg.getRawContent();
+        try {
+            msg.getType().linkedClass().cast(rq);
+            this.deliver(rq);
+        } catch (UnsupportedOperationException | ClassCastException ex) {
+            ex.printStackTrace();
         }
+    }
+
+    /**
+     * Alert that the message has no valid target
+     *
+     * @param req
+     *            the request that has not been delivered
+     * @throws OperationNotSupportedException
+     *             the operation not supported exception
+     */
+    private void deliver(RequestModel req) {
+        throw new UnsupportedOperationException();
+    }
+
+    private void deliver(JoinRequest req) {
+        this.myParty = PartyController.processJoinRequest(this.myAP, req);
+    }
+
+    private void deliver(ChatRequest req) {
+        throw new UnsupportedOperationException();
     }
 
     private boolean isJoined() {
--- src/main/java/it/polimi/ingsw/cg_30/MessageType.java
+++ src/main/java/it/polimi/ingsw/cg_30/MessageType.java
@@ -4,13 +4,25 @@ public enum MessageType {
     ChatMessage(ChatRequest.class), ActionMessage(ActionRequest.class), PartyMessage(
             PartyRequest.class), JoinMessage(JoinRequest.class);
 
+    /** The actual value of this enum constant */
     private Class c;
 
-    MessageType(Class c) {
+    /**
+     * Instantiates a new message type.
+     *
+     * @param c
+     *            the c
+     */
+    <T extends RequestModel> MessageType(Class<T> c) {
         this.c = c;
     }
 
-    public Class linkedClass() {
+    /**
+     * Linked class.
+     *
+     * @return the class
+     */
+    public <T extends RequestModel> Class<T> linkedClass() {
         return this.c;
     }
 }
